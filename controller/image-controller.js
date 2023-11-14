aca92154a81691c898208dcc959bd7194a0d7372
Kumar Mritunjay <kumarmritunjay@apples-MacBook-Air.local>
2023-11-15 00:47:43 +0530

    changes made to the project1

--- controller/image-controller.js
+++ controller/image-controller.js
@@ -1,7 +1,7 @@
 import grid from 'gridfs-stream';
 import mongoose from 'mongoose';
 
-const url = 'https://git.heroku.com/dbugle.git'   
+const url = 'https://dbugle-4ba44d25cc4b.herokuapp.com'   
 
 
 let gfs, gridfsBucket;
