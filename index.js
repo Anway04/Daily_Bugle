070fd73541967206b5905ae9e0d20614be1eebbd
Kumar Mritunjay <kumarmritunjay@apples-MacBook-Air.local>
2023-11-15 03:00:14 +0530

    initial commit

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import bodyParser from 'body-parser';
+import path from 'path';
 
 //components
 import Connection from './database/db.js';
 import Router from './routes/route.js';
+
+const __dirname = path.resolve();
 
 
 dotenv.config();
@@ -17,10 +20,14 @@ app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/', Router);
 
-if(process.env.NODE_ENV === 'production') {
-    app.use(express.static("client/build"));
-}
 
+app.use(express.static(path.join(__dirname,"./client/build")));
+
+app.get('*', function (_, res){
+    res.sendFile(path.join(__dirname,"./client/build/index.html"), function(err){
+        res.status(500).send(err);
+    })
+})
 
 const PORT = process.env.PORT || 8000;
 const username = process.env.DB_USERNAME;
