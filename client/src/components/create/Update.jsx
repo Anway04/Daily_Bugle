689cd4f51093734331093f0440c7c8d6a5e9b8db
Mritunjay-ranjan <89678656+Mritunjay-ranjan@users.noreply.github.com>
2023-11-20 19:18:15 +0530

    Update Update.jsx

--- client/src/components/create/Update.jsx
+++ client/src/components/create/Update.jsx
@@ -69,7 +69,7 @@ const Update = () => {
 
     const { id } = useParams();
 
-    const url = 'https://i.ibb.co/mB5vykx/1g-Q5b-A7y-Zd2k-TLa-UWb-Q8-Jc-RIwwc-WXCz-Pq8m1e-Y9gz6-IM2-Noy-CQQ.jpg';
+    const url = 'https://i.ibb.co/Fg57HvV/istockphoto-1313303632-640x640.jpg';
     
     useEffect(() => {
         const fetchData = async () => {
