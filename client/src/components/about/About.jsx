eeef61f49c64e3d6099a5538a97239dc07a67e9d
Mritunjay-ranjan <89678656+Mritunjay-ranjan@users.noreply.github.com>
2023-11-20 19:44:35 +0530

    Update About.jsx

--- client/src/components/about/About.jsx
+++ client/src/components/about/About.jsx
@@ -3,7 +3,7 @@ import { Box, styled, Typography, Link } from '@mui/material';
 import { GitHub, Instagram, Email } from '@mui/icons-material';
 
 const Banner = styled(Box)`
-    background-image: url(https://www.wallpapertip.com/wmimgs/23-236943_us-wallpaper-for-website.jpg);
+    background-image: url(https://www.cyberark.com/wp-content/uploads/2019/11/Developer.jpg);
     width: 100%;
     height: 50vh;
     background-position: left 0px bottom 0px;
