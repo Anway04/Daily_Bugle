58c73edaf4a4247778ece6319b2984be644eb2df
Mritunjay-ranjan <89678656+Mritunjay-ranjan@users.noreply.github.com>
2023-11-23 00:54:25 +0530

    Update Comment.jsx

--- client/src/components/details/comments/Comment.jsx
+++ client/src/components/details/comments/Comment.jsx
@@ -1,22 +1,22 @@
 import { useContext } from "react";
 
 import { Typography, Box, styled } from "@mui/material";
-import { Delete } from '@mui/icons-material';
+import { Delete } from "@mui/icons-material";
 
-import { getAccessToken } from '../../../utils/common-utils';
+import { getAccessToken } from "../../../utils/common-utils";
 
-import { API } from '../../../service/api';
+import { API } from "../../../service/api";
 import { DataContext } from "../../../context/DataProvider";
 
 const Component = styled(Box)`
-    margin-top: 30px;
-    background: #F5F5F5;
-    padding: 10px;
+  margin-top: 30px;
+  background: #f5f5f5;
+  padding: 10px;
 `;
 
 const Container = styled(Box)`
-    display: flex;
-    margin-bottom: 5px;
+  display: flex;
+  margin-bottom: 5px;
 `;
 
 const Name = styled(Typography)`
@@ -26,49 +26,36 @@ const Name = styled(Typography)`
 `;
 
 const StyledDate = styled(Typography)`
-    font-size: 14px;
-    color: #878787;
+  font-size: 14px;
+  color: #878787;
 `;
 
 const DeleteIcon = styled(Delete)`
-    margin-left: auto;
+  margin-left: auto;
 `;
 
 const Comment = ({ comment, setToggle }) => {
+  const { account } = useContext(DataContext);
 
-    const { account } = useContext(DataContext)
-    
-    const removeComment = async () => {
-       // await API.deleteComment(comment._id);
-        console.log(comment);
-        try {
-            fetch(`https://dailybuglebackend.onrender.com/comment/delete/${comment._id}`, {
-                method: 'DELETE',
-                headers: {
-                    authorization: getAccessToken(),
-                    'Content-Type': 'application/json'
-                },
-            })
-            .then(res => res.json())
-            .then(data => {
-                console.log(data);
-            })
-        } catch (error) {
-            console.log(error);
-        }
-       setToggle(prev => !prev);
+  const removeComment = async () => {
+    let response = await API.deleteComment(comment._id);
+    if (response.isSuccess) {
+      setToggle((prev) => !prev);
     }
+  };
 
-    return (
-        <Component>
-            <Container>
-                <Name>{comment.name}</Name>
-                <StyledDate>{new Date(comment.date).toDateString()}</StyledDate>
-                { comment.name === account.username && <DeleteIcon onClick={() => removeComment()} /> }
-            </Container>
-            <Typography>{comment.comments}</Typography>
-        </Component>
-    )
-}
+  return (
+    <Component>
+      <Container>
+        <Name>{comment.name}</Name>
+        <StyledDate>{new Date(comment.date).toDateString()}</StyledDate>
+        {comment.name === account.username && (
+          <DeleteIcon onClick={() => removeComment()} />
+        )}
+      </Container>
+      <Typography>{comment.comments}</Typography>
+    </Component>
+  );
+};
 
 export default Comment;
